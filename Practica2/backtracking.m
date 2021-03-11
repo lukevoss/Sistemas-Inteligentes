57097a756a0e48a44da8b6a3d783f18034558a6c
lukevoss <luke@familievoss.net>
2021-03-11 10:04:01 +0100

    Done

--- Practica2/backtracking.m
+++ Practica2/backtracking.m
@@ -5,6 +5,7 @@ function [x,domains] = backtracking(x, domains, case_noassign, case_ordered)
         withoutValue = find(x == 0);
         while ~isempty(valuesList) && ~isempty(withoutValue)
             value = valuesList(1);
+            valuesList(1)=[];
             if isSafe(x,value,v)
                 domains_bevor = domains;
                 [x,domains] = assign_value(x,domains,v,value);
@@ -14,8 +15,6 @@ function [x,domains] = backtracking(x, domains, case_noassign, case_ordered)
                     x(v) = 0;
                     domains = domains_bevor;
                 end
-            else
-                valuesList(1)=[];
             end
         end
     end
