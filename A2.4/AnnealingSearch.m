50316dacc7cdbcabb7504ead6ca4ccce1410fed7
Henri <henri.smidt@t-online.de>
2021-03-25 16:48:35 +0100

    Annealing working

--- A2.4/AnnealingSearch.m
+++ A2.4/AnnealingSearch.m
@@ -1,13 +1,16 @@
 function [Current, T_plot, x_plot, cost_plot, successor_plot, accepted_plot] = AnnealingSearch(A,inicial,T,T_min,N)
 %Anealing Search for worker-task problem
+[T_plot, x_plot, cost_plot, successor_plot, accepted_plot] = deal([]);
 Current = inicial;
 noImprovement = 0;
+iter = 1;
 while (T>T_min && noImprovement < 20 && cost(A,Current,1)~=0)
     T_plot = [T_plot, T];
     New = randSuccessor(Current, N, 1);
-    x_plot = [x_plot, Current];
+    x_plot(iter,1:length(Current)) = Current;
     cost_plot = [cost_plot, cost(A,Current,1)];
-    successor_plot = [successor_plot, New];
+    successor_plot(iter, 1:length(New)) = New;
+    iter = iter + 1;
     deltaE = cost(A,New,1)  - cost(A,Current,1);
     if deltaE<0
         Current=New;
@@ -19,9 +22,10 @@ while (T>T_min && noImprovement < 20 && cost(A,Current,1)~=0)
             Current=New;
             noImprovement = 0;
             accepted_plot = [accepted_plot, 1];
+        else
+            noImprovement = noImprovement + 1;
+            accepted_plot = [accepted_plot, 0];
         end
-        noImprovement = noImprovement + 1;
-        accepted_plot = [accepted_plot, 0];
     end
     T = enfriar(T);
 end
