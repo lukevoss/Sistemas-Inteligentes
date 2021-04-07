c2ea35660e3d30029a8260cdf0abe4bbad34bb45
lukevoss <luke@familievoss.net>
2021-04-08 01:00:04 +0200

    Done

--- A2.5/AnnealingSearch.m
+++ A2.5/AnnealingSearch.m
@@ -1,24 +1,34 @@
-function [Current, T_plot] = AnnealingSearch(T,T_min,N)
+function [solutionFound, iterations] = AnnealingSearch(T,T_min,N, start)
 %Anealing Search for N-Queens Problem
-Current = randperm(N);
+Current = start;
 noImprovement = 0;
-T_plot = T;
+iterations = 0;
+currentCost = fEval(Current);
+
 while (T>T_min && noImprovement < 20 && fEval(Current)~=0)
+    iterations = iterations +1;
     New = randSuccessor(Current, N, 1);
-    deltaE = fEval(New)  - fEval(Current);
+    NewCost = fEval(New);
+    deltaE = NewCost  - currentCost;
     if deltaE<0
         Current=New;
+        currentCost = NewCost;
         noImprovement = 0;
     else
         p=exp(-deltaE/T);
         if p>rand()
             Current=New;
+            currentCost = NewCost;
             noImprovement = 0;
         end
         noImprovement = noImprovement + 1;
     end
     T = enfriar(T);
-    T_plot = [T_plot, T];
+end
+if (currentCost == 0)
+    solutionFound = true;
+else
+    solutionFound = false;
 end
 end
 
