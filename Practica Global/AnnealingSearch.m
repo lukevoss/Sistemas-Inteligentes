c4dd3489b782f40caed40303d88c0c09b1484c04
Henri <henri.smidt@t-online.de>
2021-04-25 16:41:05 +0200

    bla

---
+++ Practica Global/AnnealingSearch.m
@@ -0,0 +1,28 @@
+function [bestCost, iter] = AnnealingSearch(satelites,installCosts,representatives)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+T=1000;
+T_min=0.1;
+noImprovement = 0;
+iter = 0;
+Current=representatives;
+while (T>T_min && noImprovement < 20)
+    New = AnnealingSuccessor(Current, satelites, installCosts);
+    deltaE = fEval(satelites, New)  - fEval(satelites, Current);
+    if deltaE<0
+        Current=New;
+        noImprovement = 0;
+    else
+        p=exp(-deltaE/T);
+        if p>rand()
+            Current=New;
+            noImprovement = 0;
+        end
+        noImprovement = noImprovement + 1;
+    end
+    T = enfriar(T);
+    iter = iter + 1;
+end
+bestCost=fEval(satelites, Current);
+end
+
